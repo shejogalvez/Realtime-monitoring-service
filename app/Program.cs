730f887d3f1312f1916bfe196cb253daf98632ef
shejogalvez <sergio.galvez@ug.uchile.cl>
2026-01-02 16:35:56 -0300

    add main loop
    subscribe enabled robots to WeatherDataReceiver signal and read data from user,
    receive configuration path from command line arguments

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using app.bots;
+using app;
+
+// obtain filepath as command line argument or use default
+string ConfigurationFilePath = args.ElementAtOrDefault(0) ?? "configuration.json";
+
+// initialize weather bots from configuration file
+var configReader = new ConfigurationReader(ConfigurationFilePath);
+List<WeatherBot> bots = configReader.GetBotsFromConfiguration();
+
+// subscribe bots to incoming weather data
+var DataManager = new WeatherDataReceiver();
+foreach (var bot in bots)
+{
+    DataManager.WeatherDataReceived += bot.OnWeatherDataReceived;
+}
+
+// read weather data
+while (true)
+{
+    Console.WriteLine("\ninput weather data: ");
+    DataManager.ReadFromUser();
+}
